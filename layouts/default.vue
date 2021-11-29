c08aa87574c7c9d8d8f47f8f1902ad0026053e55
sonseong10 <sonseong10@gmail.com>
2021-11-29 18:30:26 +0900

    🚧 Implement NuxtLink

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h1>{{ $route.name }}템플릿</h1>
+    <header>
+      <ul>
+        <li>
+          <NuxtLink to="/">홈</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/main">메인</NuxtLink>
+        </li>
+      </ul>
+    </header>
     <Nuxt />
   </div>
 </template>
