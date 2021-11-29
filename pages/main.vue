250117fca6b65f3478fc6dd7ebf47aa067c191b0
sonseong10 <sonseong10@gmail.com>
2021-11-29 17:09:54 +0900

    🚧 Implement route component: main

---
+++ pages/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Logo />
+    <h1>메인 페이지</h1>
+  </div>
+</template>
+
+<script>
+import Logo from '@/components/NuxtLogo.vue'
+
+export default {
+  components: {
+    Logo,
+  },
+}
+</script>
+
+<style></style>
